c849967fc72222a5d8e1c5e537a04aef420dde70
blechdavier <blechdavier@gmail.com>
2024-03-15 01:01:13 -0400

    remove nannou visualizations in order to run on pi

--- src/main.rs
+++ src/main.rs
@@ -1,216 +1,107 @@
-use nannou::{prelude::*, winit::event::DeviceEvent};
 use serialport::SerialPort;
 use std::time::{Duration, Instant};
 
-struct Model {
-    /// The serial port to which the lidar is connected.
-    port: Box<dyn SerialPort>,
-    scan_packets: Vec<ScanPacket>,
-    scans: Vec<Vec<LidarPoint>>,
-    zoom: f32,
-}
-
 fn main() {
-    nannou::app(model)
-        .event(event)
-        .simple_window(view)
-        .size(600, 600)
-        .run();
-}
-
-fn model(_app: &App) -> Model {
-    let mut port = serialport::new("COM3", 115_200)
+    // sudo apt-get install -y libudev-dev
+    let mut port = serialport::new("/dev/ttyAMA0", 115_200)
         .timeout(Duration::from_millis(10))
         .open()
         .expect("Failed to open port");
     initialize_lidar(&mut port);
-    Model {
-        port,
-        scan_packets: Vec::new(),
-        scans: Vec::new(),
-        zoom: 1.0,
-    }
-}
+    let mut scan_packets = Vec::new();
+    let mut scans = Vec::new();
 
-fn event(_app: &App, model: &mut Model, event: Event) {
-    // zoom on scroll
-    match event {
-        Event::Update(_update) => {
-            let port = &mut model.port;
-            while port.bytes_to_read().unwrap() >= 132 {
-                let scan: Vec<u8> = read(port, 132).unwrap();
-                model.scan_packets.push(deserialize_express_scan_extended(
-                    scan.as_slice().try_into().unwrap(),
-                ));
-                if model.scan_packets.len() > 1 {
-                    let previous_scan = &model.scan_packets[model.scan_packets.len() - 2];
-                    let current_scan = &model.scan_packets[model.scan_packets.len() - 1];
-                    for i in 0..32 {
-                        let mut dist_q2 = [0; 3];
-                        // let mut quality = [0; 3];
-                        // let mut sync_bit = [false; 3];
+    loop {
+        while port.bytes_to_read().unwrap() >= 132 {
+            let scan: Vec<u8> = read(&mut port, 132).unwrap();
+            scan_packets.push(deserialize_express_scan_extended(
+                scan.as_slice().try_into().unwrap(),
+            ));
+            if scan_packets.len() > 1 {
+                let previous_scan = &scan_packets[scan_packets.len() - 2];
+                let current_scan = &scan_packets[scan_packets.len() - 1];
+                for i in 0..32 {
+                    let mut dist_q2 = [0; 3];
+                    // let mut quality = [0; 3];
+                    // let mut sync_bit = [false; 3];
 
-                        let combined_x3 = previous_scan.ultra_cabins[i];
-                        // unpack
-                        let dist_major1 = combined_x3 & 0xFFF;
-                        let mut dist_predict1 = ((combined_x3 as i32) << 10) >> 22;
-                        let mut dist_predict2 = (combined_x3 as i32) >> 22;
+                    let combined_x3 = previous_scan.ultra_cabins[i];
+                    // unpack
+                    let dist_major1 = combined_x3 & 0xFFF;
+                    let mut dist_predict1 = ((combined_x3 as i32) << 10) >> 22;
+                    let mut dist_predict2 = (combined_x3 as i32) >> 22;
 
-                        let dist_major2 = if i == 31 {
-                            current_scan.ultra_cabins[0] & 0xFFF
-                        } else {
-                            previous_scan.ultra_cabins[i + 1] & 0xFFF
+                    let dist_major2 = if i == 31 {
+                        current_scan.ultra_cabins[0] & 0xFFF
+                    } else {
+                        previous_scan.ultra_cabins[i + 1] & 0xFFF
+                    };
+
+                    let mut scale_level1 = 0;
+                    let mut scale_level2 = 0;
+
+                    let dist_major1 = varbitscale_decode(dist_major1, &mut scale_level1);
+                    let dist_major2 = varbitscale_decode(dist_major2, &mut scale_level2);
+
+                    let mut dist_base1 = dist_major1;
+                    let dist_base2 = dist_major2;
+
+                    if dist_major1 == 0 && dist_major2 != 0 {
+                        dist_base1 = dist_major2;
+                        scale_level1 = scale_level2;
+                    }
+
+                    dist_q2[0] = dist_major1 << 2;
+                    if dist_predict1 as u32 == 0xFFFFFE00 || dist_predict1 == 0x1FF {
+                        dist_q2[1] = 0
+                    } else {
+                        dist_predict1 <<= scale_level1;
+                        dist_q2[1] = ((dist_base1 as i32 + dist_predict1) << 2) as u32;
+                    }
+
+                    if dist_predict2 as u32 == 0xFFFFFE00 || dist_predict2 == 0x1FF {
+                        dist_q2[2] = 0
+                    } else {
+                        dist_predict2 <<= scale_level2;
+                        dist_q2[2] = ((dist_base2 as i32 + dist_predict2) << 2) as u32;
+                    }
+
+                    let start_angle_q6 = previous_scan.start_angle_q6 as i32;
+                    let angle_diff_q6 = (current_scan.start_angle_q6 as i32
+                        - start_angle_q6 as i32)
+                        .rem_euclid(360 * 64);
+
+                    if scans.len() == 0 {
+                        scans.push(Vec::new());
+                    }
+                    for j in 0..3 {
+                        let point = LidarPoint {
+                            angle_q6: start_angle_q6 as u16
+                                + (angle_diff_q6 as f64 * (i as f64 / 32.0 + j as f64 / 96.0))
+                                    as u16,
+                            distance_q2: dist_q2[j],
                         };
 
-                        let mut scale_level1 = 0;
-                        let mut scale_level2 = 0;
+                        scans[0].push(point);
 
-                        let dist_major1 = varbitscale_decode(dist_major1, &mut scale_level1);
-                        let dist_major2 = varbitscale_decode(dist_major2, &mut scale_level2);
-
-                        let mut dist_base1 = dist_major1;
-                        let dist_base2 = dist_major2;
-
-                        if dist_major1 == 0 && dist_major2 != 0 {
-                            dist_base1 = dist_major2;
-                            scale_level1 = scale_level2;
-                        }
-
-                        dist_q2[0] = dist_major1 << 2;
-                        if dist_predict1 as u32 == 0xFFFFFE00 || dist_predict1 == 0x1FF {
-                            dist_q2[1] = 0
-                        } else {
-                            dist_predict1 <<= scale_level1;
-                            dist_q2[1] = ((dist_base1 as i32 + dist_predict1) << 2) as u32;
-                        }
-
-                        if dist_predict2 as u32 == 0xFFFFFE00 || dist_predict2 == 0x1FF {
-                            dist_q2[2] = 0
-                        } else {
-                            dist_predict2 <<= scale_level2;
-                            dist_q2[2] = ((dist_base2 as i32 + dist_predict2) << 2) as u32;
-                        }
-
-                        let start_angle_q6 = previous_scan.start_angle_q6 as i32;
-                        let angle_diff_q6 = (current_scan.start_angle_q6 as i32
-                            - start_angle_q6 as i32)
-                            .rem_euclid(360 * 64);
-
-                        if model.scans.len() == 0 {
-                            model.scans.push(Vec::new());
-                        }
-                        for j in 0..3 {
-                            let point = LidarPoint {
-                                angle_q6: start_angle_q6 as u16
-                                    + (angle_diff_q6 as f64 * (i as f64 / 32.0 + j as f64 / 96.0))
-                                        as u16,
-                                distance_q2: dist_q2[j],
-                            };
-
-                            model.scans[0].push(point);
-
-                            // // a scan is defined by 360 degrees
-                            // let scan = model.scans.last_mut().unwrap();
-                            // let first_point = scan.first();
-                            // if let Some(first_point) = first_point {
-                            //     let angle_diff =
-                            //         (point.angle_q6 as i32 - first_point.angle_q6 as i32).abs();
-                            //     if angle_diff > 360 * 64 / 2 {
-                            //         model.scans.push(Vec::new());
-                            //     }
-                            // } else {
-                            //     scan.push(point);
-                            // }
-                        }
+                        // // a scan is defined by 360 degrees
+                        // let scan = scans.last_mut().unwrap();
+                        // let first_point = scan.first();
+                        // if let Some(first_point) = first_point {
+                        //     let angle_diff =
+                        //         (point.angle_q6 as i32 - first_point.angle_q6 as i32).abs();
+                        //     if angle_diff > 360 * 64 / 2 {
+                        //         scans.push(Vec::new());
+                        //     }
+                        // } else {
+                        //     scan.push(point);
+                        // }
                     }
                 }
             }
-            println!("scans: {}", model.scans.len());
         }
-        Event::WindowEvent {
-            simple: Some(event),
-            ..
-        } => match event {
-            nannou::event::WindowEvent::MouseWheel(delta, _) => match delta {
-                nannou::event::MouseScrollDelta::LineDelta(_, y) => {
-                    model.zoom *= 1.0 + y * 0.1;
-                }
-                nannou::event::MouseScrollDelta::PixelDelta(_) => (),
-            },
-            _ => (),
-        },
-        _ => (),
+        println!("scan_packets: {}", scan_packets.len());
     }
-}
-
-fn view(app: &App, model: &Model, frame: Frame) {
-    let draw = app.draw();
-    // minimum of the width and height of the window
-    let scale = app.window_rect().w().min(app.window_rect().h()) * 0.45;
-    let angle = match model.scan_packets.last() {
-        Some(scan_packet) => {
-            scan_packet.start_angle_q6 as f32 / 64.0 * std::f32::consts::PI / 180.0
-        }
-        None => 0.0,
-    };
-    draw.background().color(WHITE);
-    // draw a circle in the middle of the window
-    draw.ellipse()
-        .x_y(0.0, 0.0)
-        .radius(scale)
-        .stroke(LIGHTGRAY)
-        .stroke_weight(2.0);
-    draw.ellipse().x_y(0.0, 0.0).color(LIGHTGRAY).radius(2.0);
-    draw.line()
-        .start(pt2(0.0, -scale))
-        .end(pt2(0.0, scale))
-        .color(LIGHTGRAY)
-        .stroke_weight(2.0);
-    draw.line()
-        .start(pt2(-scale, 0.0))
-        .end(pt2(scale, 0.0))
-        .color(LIGHTGRAY)
-        .stroke_weight(2.0);
-    draw.line()
-        .start(pt2(0.0, 0.0))
-        .end(pt2(angle.sin() * scale, angle.cos() * scale))
-        .color(RED)
-        .stroke_weight(2.0);
-    // mouse line
-    let mouse = app.mouse.position();
-    let mag = (mouse.x.powi(2) + mouse.y.powi(2)).sqrt().max(1.0);
-    draw.line()
-        .start(pt2(0.0, 0.0))
-        .end(pt2(mouse.x / mag * scale, mouse.y / mag * scale))
-        .color(LIGHTGRAY)
-        .stroke_weight(2.0);
-
-    draw.text(&format!("angle: {}", angle))
-        .x_y(0.0, 0.0)
-        .color(BLACK)
-        .font_size(24);
-
-    let mut points = 0;
-    // call a scan the last 200 points
-    if let Some(scan) = model.scans.last() {
-        for point in scan.iter().rev().take(200) {
-            let angle = point.angle_q6 as f32 / 64.0 * std::f32::consts::PI / 180.0;
-            let distance = point.distance_q2 as f32 / 4.0;
-            let x = angle.sin() * distance * model.zoom;
-            let y = angle.cos() * distance * model.zoom;
-            draw.ellipse()
-                .x_y(x, y)
-                .radius(2.0)
-                .color(BLACK)
-                .stroke_weight(0.0);
-            points += 1;
-        }
-    }
-    draw.text(&format!("points: {}", points))
-        .x_y(0.0, -scale * 0.9)
-        .color(BLACK)
-        .font_size(24);
-    draw.to_frame(app, &frame).unwrap();
 }
 
 fn initialize_lidar(port: &mut Box<dyn SerialPort>) {
@@ -221,12 +112,20 @@ fn initialize_lidar(port: &mut Box<dyn SerialPort>) {
     // clear buffer
     port.clear(serialport::ClearBuffer::Input).unwrap();
     // wait
-    std::thread::sleep(Duration::from_millis(500));
-    write(port, [0xa5, 0x52]);
-    assert_eq!(
-        read(port, 10).unwrap(),
-        [0xa5, 0x5a, 0x03, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00]
-    );
+    for _ in 0..10 {
+        std::thread::sleep(Duration::from_millis(500));
+        write(port, [0xa5, 0x52]);
+        std::thread::sleep(Duration::from_millis(500));
+        if let Ok(response) = read(port, 10) {
+            assert_eq!(
+                response,
+                [0xa5, 0x5a, 0x03, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00]
+            );
+            break;
+        } else {
+            println!("No response, retrying in 1 second");
+        }
+    }
     write(port, [0xa5, 0x50]);
     // a5 5a 14 00 00 00 04 18 1d 01 07 d9 8a 99 f6 c9 e5 9a d2 c5 e5 9c f7 25 58 34 12
     assert_eq!(
